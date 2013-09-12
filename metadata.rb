0a60ec233058d9c9557d4dac4c43a69d0d88c70e
Youssuf ElKalay <yelkalay@gmail.com>
2013-09-11 22:00:54 -0700

    Initial commit with scaffolding for chef cookbook and berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-log-tail'
+maintainer       'buildscientist.com'
+maintainer_email 'buildscientist@gmail.com'
+license          'Apache v2.0 License'
+description      'Installs/Configures chef-log-tail'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
